4dfbc4e3270025a548e2ddc6e5f15cb5e04fd9b5
Huw Diprose <mail@huwdiprose.co.uk>
2025-11-11 17:27:59 +0000

    Overhaul the check_wikidata_book_entries script
    Returns a todo list of Mandatory and other fields I can use to target
    work.
    
    Initial focus is on Works, Editions is somewhat broken, but i should
    be able to repuprose a lot of this.
    
    key difference is that I want to do this:
    
    ```ruby
             if instance_of == "Q106833"
               {
                 item["id"] => {
                   "Missing Mandatory" => {
                     "change instance_of" => {
                       "is_currently" => "Q106833",
                       "should_be" => "Q122731938"
                     }
                   }
                 }
               }
    ```
    
    That is, I want to rename a bunch of existing audiobooks to audiobook
    editions

--- scripts/check_wikidata_books_entries.rb
+++ scripts/check_wikidata_books_entries.rb
@@ -2,444 +2,129 @@ require 'json'
 require 'httparty'
 require 'yaml'
 require 'pry'
+require_relative './support/helpers'
+require_relative './data_model'
+
+class Skippable
+  def works
+    [
+      "Q114051262", # Tweet of the day, no written work, was a Radio Programme
+      "Q756604"     # Persepolis, no written work, is comic serise
+    ]
+  end
+
+  def editions
+    []
+  end
+end
 
 class CheckWikidataBookEntries
-  WIKIDATA_REST_GET_ITEM = "https://www.wikidata.org/w/rest.php/wikibase/v0/entities/items"
+  include Helpers
 
   def initialize
-    @books = Dir.glob("_books/*.md").map do |file|
-      frontmatter = File.read(file)[/\A---\s*\n(.*?)\n---/m, 1]
-      YAML.safe_load(frontmatter || "", permitted_classes: [Time], aliases: true) || {}
+    @books = load_stored_books
+  end
+
+  def validate_item(type)
+    items_to_validate = book_iris("#{type}_iri")
+    wikidata_items = query_wikidata(items_to_validate, type)
+
+    available_items = wikidata_items.reject { |item| item.code != 200 }
+
+    available_item_details = available_items.map.with_index do |item, i|
+      validation_against_schema(item.parsed_content, type)
     end
+
+    return {
+      available: available_item_details.compact,
+    }
   end
 
-  def works_iris
-    @books.select do |book|
-      book["work_iri"] != nil && book["work_iri"]&.split("/")&.count > 4
-    end.map { |book| book["work_iri"].split("/").last }
-  end
+  private
 
-  def edition_iris
-    @books.select do |book|
-      book["edition_iri"] != nil && book["edition_iri"]&.split("/")&.count > 4
-    end.map { |book| book["edition_iri"].split("/").last }
-  end
+  def format_return_payload(item, validation_schema)
+    return {} if validation_schema.nil?
 
-  def query_wikidata(iris)
-    print
-    iris.map.with_index do |iri, i|
-      print "\rQuerying #{i + 1}/#{iris.count}"
+    missing_mandatory = validation_schema["Mandatory"].keys - item["statements"].keys
+    missing_conditionally_mandatory = validation_schema["Mandatory if applicable"].keys - item["statements"].keys
+    missing_optional = validation_schema["Optional"].keys - item["statements"].keys
 
-      response = HTTParty.get(
-        "#{WIKIDATA_REST_GET_ITEM}/#{iri}",
-        headers: { 'accept' => 'application/json' }
-      )
-
-      unless response.code == 200
-        raise "\nWikidata query failed for #{iri} (status #{response.code})"
-      end
-
-      response.parsed_response
-    end
-  end
-
-  def validate_work
-    print "Validating Works:\n"
-    work_items = query_wikidata(works_iris)
-    available_items = work_items.reject { |item| item["httpCode"] == 404 }
-    available_items.map.with_index do |work, i|
-      print "\r"
-      print "Validating #{i+1}/#{work_items.count}"
-      instance_of = work["statements"]["P31"].map { |s| s["value"]["content"] }.first
-      unless ["Q114051262", "Q756604"].include?(work["id"])
-        binding.pry if !validation_data.keys.include?(instance_of)
-      end
-      schema = validation_data[instance_of]
-      if schema.nil?
-        {}
-      else
-        {
-          work["id"] => {
-            "labels" => { **work["labels"] },
-            "Missing Mandatory" => schema["Mandatory"].keys - work["statements"].keys,
-          }
-        }
-      end
-    end
-    unavailable_items = work_items.select { |item| item["httpCode"] == 404 }
-    unless unavailable_items.empty?
-      print "There were also #{unavailable_items.count} unavailable items:\n"
-      unavailable_items.map do |item|
-        print "\r"
-        print "Unavailable #{item["id"]}"
-        {
-          item["id"] => {
-            "labels" => { **item["labels"] },
-            "Missing Mandatory" => ["P31", "P1476", "P50", "P577", "P407", "P747"]
-          }
-        }
-      end
-    end
-  end
-
-  def validate_edition
-    print "Validating Editions:\n"
-    items = query_wikidata(edition_iris)
-    items.map.with_index do |item, i|
-      print "\r"
-      print "Validating #{i+1}/#{items.count}"
-      instance_of = item["statements"]["P31"].map { |s| s["value"]["content"] }.first
-      unless [].include?(item["id"]) || instance_of == "Q106833"
-        binding.pry if !validation_data.keys.include?(instance_of)
-      end
-      schema = validation_data[instance_of]
-      if schema.nil?
-        {}
-      else
-        if instance_of == "Q106833"
-          {
-            item["id"] => {
-              "Missing Mandatory" => {
-                "change instance_of" => {
-                  "is_currently" => "Q106833",
-                  "should_be" => "Q122731938"
-                }
-              }
-            }
-          }
-        else
-          {
-            item["id"] => {
-              "labels" => { **item["labels"] },
-              "Missing Mandatory" => schema["Mandatory"].keys - item["statements"].keys,
-            }
-          }
-        end
-      end
-    end
-  end
-
-  def validation_data
     {
-      "Q7725634" => {
-        "Mandatory" => {
-          "P31" => "instance of",
-          "P1476" => "title",
-          "P50" => "author",
-          "P407" => "language of work or name",
-          "P577" => "publication date",
-          "P747" => "has edition or translation"
-        },
-        "Mandatory if applicable" => {
-          "P1680" => {
-            "label" => "subtitle",
-            "condition" => "if present"
-          },
-          "P767" => {
-            "label" => "contributor to the creative work or subject",
-            "condition" => "if present"
-          },
-          "P155" => {
-            "label" => "follows",
-            "condition" => "if present"
-          },
-          "P156" => {
-            "label" => "followed by",
-            "condition" => "if present"
-          },
-          "P571" => {
-            "label" => "inception",
-            "condition" => "if known"
-          }
-        },
-        "Optional" => {
-          "P136" => "genre",
-          "P135" => "movement",
-          "P921" => "main subject",
-          "P674" => "characters",
-          "P840" => "narrative location",
-          "P144" => "based on",
-          "P941" => "inspired by"
-        }
-      },
-      "Q35760" => {
-        "Mandatory" => {
-          "P31" => "instance of",
-          "P1476" => "title",
-          "P577" => "publication date",
-          "P50" => "author",
-          "P407" => "language of work or name",
-          "P747" => "has edition or translation"
-        },
-        "Mandatory if applicable" => {
-          "P1680" => {
-            "label" => "subtitle",
-            "condition" => "if present"
-          },
-          "P767" => {
-            "label" => "contributor to the creative work or subject",
-            "condition" => "if present"
-          },
-          "P155" => {
-            "label" => "follows",
-            "condition" => "if present"
-          },
-          "P156" => {
-            "label" => "followed by",
-            "condition" => "if present"
-          },
-          "P571" => {
-            "label" => "inception",
-            "condition" => "if known"
-          }
-        },
-        "Optional" => {
-          "P136" => "genre",
-          "P135" => "movement",
-          "P921" => "main subject",
-          "P674" => "characters",
-          "P840" => "narrative location",
-          "P144" => "based on",
-          "P941" => "inspired by"
-        }
-      },
-      "Q725377" => {
-        "Mandatory" => {
-          "P31" => "instance of",
-          "P1476" => "title",
-          "P577" => "publication date",
-          "P50" => "author",
-          "P747" => "has edition or translation",
-          "P407" => "language of work or name"
-        },
-        "Mandatory if applicable" => {
-          "P1680" => {
-            "label" => "subtitle",
-            "condition" => "if present"
-          },
-          "P767" => {
-            "label" => "contributor to the creative work or subject",
-            "condition" => "if present"
-          },
-          "P155" => {
-            "label" => "follows",
-            "condition" => "if present"
-          },
-          "P156" => {
-            "label" => "followed by",
-            "condition" => "if present"
-          },
-          "P571" => {
-            "label" => "inception",
-            "condition" => "if known"
-          }
-        },
-        "Optional" => {
-          "P136" => "genre",
-          "P135" => "movement",
-          "P921" => "main subject",
-          "P674" => "characters",
-          "P840" => "narrative location",
-          "P144" => "based on",
-          "P941" => "inspired by"
-        }
-      },
-      "Q8261" => {
-        "Mandatory" => {
-          "P31" => "instance of",
-          "P1476" => "title",
-          "P577" => "publication date",
-          "P50" => "author",
-          "P407" => "language of work or name",
-          "P747" => "has edition or translation",
-        },
-        "Mandatory if applicable" => {
-          "P1680" => {
-            "label" => "subtitle",
-            "condition" => "if present"
-          },
-          "P767" => {
-            "label" => "contributor to the creative work or subject",
-            "condition" => "if present"
-          },
-          "P155" => {
-            "label" => "follows",
-            "condition" => "if present"
-          },
-          "P156" => {
-            "label" => "followed by",
-            "condition" => "if present"
-          },
-          "P571" => {
-            "label" => "inception",
-            "condition" => "if known"
-          }
-        },
-        "Optional" => {
-          "P136" => "genre",
-          "P135" => "movement",
-          "P921" => "main subject",
-          "P674" => "characters",
-          "P840" => "narrative location",
-          "P144" => "based on",
-          "P941" => "inspired by"
-        }
-      },
-      "Q47461344" => {
-        "Mandatory" => {
-          "P31" => "instance of",
-          "P1476" => "title",
-          "P577" => "publication date",
-          "P50" => "author",
-          "P407" => "language of work or name",
-          "P747" => "has edition or translation",
-        },
-        "Mandatory if applicable" => {
-          "P1680" => {
-            "label" => "subtitle",
-            "condition" => "if present"
-          },
-          "P767" => {
-            "label" => "contributor to the creative work or subject",
-            "condition" => "if present"
-          },
-          "P155" => {
-            "label" => "follows",
-            "condition" => "if present"
-          },
-          "P156" => {
-            "label" => "followed by",
-            "condition" => "if present"
-          },
-          "P571" => {
-            "label" => "inception",
-            "condition" => "if known"
-          }
-        },
-        "Optional" => {
-          "P136" => "genre",
-          "P135" => "movement",
-          "P921" => "main subject",
-          "P674" => "characters",
-          "P840" => "narrative location",
-          "P144" => "based on",
-          "P941" => "inspired by"
-        }
-      },
-      "Q122731938" => {
-        "Mandatory" => {
-          "P31" => "instance of",
-          "P629" => "edition or translation of",
-          "P577" => "publication date",
-          "P123" => "publisher",
-          "P407" => "language of work or name",
-          "P2438" => "narrator",
-          "P2047" => "duration"
-        },
-        "Mandatory if applicable" => {
-          "P2679" => {
-            "label" => "author of foreword",
-            "condition" => "if present"
-          },
-          "P2680" => {
-            "label" => "author of afterword",
-            "condition" => "if present"
-          },
-          "P98" => {
-            "label" => "editor",
-            "condition" => "if present"
-          },
-        },
-        "Mandatory if different from work" => {
-          "P50" => "author",
-          "P1476" => "title",
-          "P1680" => "subtitle",
-        },
-        "Optional" => {
-          "P655" => "translator"
-        }
-      },
-      "Q3331189" => {
-        "Mandatory" => {
-          "P31" => "instance of",
-          "P629" => "edition or translation of",
-          "P1476" => "title",
-          "P291" => "place of publication",
-          "P577" => "publication date",
-          "P123" => "publisher",
-          "P407" => "language of work or name"
-        },
-        "Mandatory if applicable" => {
-          "P2679" => {
-            "label" => "author of foreword",
-            "condition" => "if present"
-          },
-          "P2680" => {
-            "label" => "author of afterword",
-            "condition" => "if present"
-          },
-          "P98" => {
-            "label" => "editor",
-            "condition" => "if present"
-          },
-        },
-        "Mandatory if different from work" => {
-          "P50" => "author",
-          "P1476" => "title",
-          "P1680" => "subtitle",
-        },
-        "Optional" => {
-          "P655" => "translator",
-          "P110" => "illustrator",
-          "P872" => "printed by",
-        }
-      },
-      "Q1224889" => {
-        "Mandatory" => {
-          "P31" => "instance of",
-          "P629" => "edition or translation of",
-          "P1476" => "title",
-          "P291" => "place of publication",
-          "P577" => "publication date",
-          "P123" => "publisher",
-          "P407" => "language of work or name"
-        },
-        "Mandatory if applicable" => {
-          "P2679" => {
-            "label" => "author of foreword",
-            "condition" => "if present"
-          },
-          "P2680" => {
-            "label" => "author of afterword",
-            "condition" => "if present"
-          },
-          "P98" => {
-            "label" => "editor",
-            "condition" => "if present"
-          },
-        },
-        "Mandatory if different from work" => {
-          "P50" => "author",
-          "P1476" => "title",
-          "P1680" => "subtitle",
-        },
-        "Optional" => {
-          "P655" => "translator",
-          "P110" => "illustrator",
-          "P872" => "printed by",
-        }
+      item["id"] => {
+        "labels" => { **item["labels"].select { |k, v| k == "en" } },
+        "Missing Mandatory" => missing_mandatory.compact,
+        "Mandatory if applicable" => missing_conditionally_mandatory.compact,
+        "Optional" => missing_optional.compact
       }
     }
+  end
+
+  def item_instance(item)
+    item.dig("statements", "P31", 0, "value", "content")
+  end
+
+  def book_iris(key)
+    @books.values
+      .map { |book| book[key] }
+      .select { |iri| complete_wikidata_iri?(iri) }
+      .map { |iri| iri.split("/").last }
+      .uniq
+  end
+
+  def query_wikidata(iris, type)
+    print
+    iris.map.with_index do |iri, i|
+      print "\rQuerying Wikidata #{type.capitalize} #{i + 1}/#{iris.count}"
+      wikidata_rest_client.get_item(iri)
+    end
+  end
+
+  def validation_against_schema(item, type)
+    instance_of = item_instance(item)
+
+    if unknown_instance_type?(instance_of, type) && !not_known_edgecase_work?(item["id"], type)
+      raise UnknownInstanceType
+    end
+
+    validation_schema = validation_data(type)[instance_of]
+    return format_return_payload(item, validation_schema)
+
+  rescue UnknownInstanceType
+    binding.pry
+  end
+
+  def not_known_edgecase_work?(work_id, type)
+    case type
+    when "work"
+      !Skippable.new.works.include?(work_id)
+    when "edition"
+      !Skippable.new.editions.include?(work_id)
+    end
+  end
+
+  def unknown_instance_type?(instance_type, allowable_type)
+    validation_data(allowable_type).keys.include?(instance_type)
+  end
+
+  def validation_data(type)
+    data_model = DataModel.new
+
+    case type
+    when "work"
+      data_model.work_types
+    when "edition"
+      data_model.edition_types
+    else
+      raise UnknownInstanceType
+    end
   end
 end
 
 entry_checker = CheckWikidataBookEntries.new
-output_works = entry_checker.validate_work
-print "\n"
-todo_works = output_works.nil? ? {} : output_works.reject { |work| work == {} }.select { |work| work.values.first["Missing Mandatory"] != [] }
+output_works = entry_checker.validate_item("work")
 
-output_editions = entry_checker.validate_edition
-print "\n"
-todo_editions = output_editions.nil? ? {} : output_editions.reject { |ed| ed == {} }.compact.select { |ed| ed.values.first["Missing Mandatory"] != [] }
-File.open('_data/wikidata_works_to_improve.json', 'w') { |file| file.write(JSON.pretty_generate(todo_works)) }
-File.open('_data/wikidata_edition_to_improve.json', 'w') { |file| file.write(JSON.pretty_generate(todo_editions)) }
+# output_editions = entry_checker.validate_edition
+# print "\n"
+# todo_editions = output_editions.nil? ? {} : output_editions.reject { |ed| ed == {} }.compact.select { |ed| ed.values.first["Missing Mandatory"] != [] }
+File.open('_data/wikidata_works_to_improve.json', 'w') { |file| file.write(JSON.pretty_generate(output_works)) }
+# File.open('_data/wikidata_edition_to_improve.json', 'w') { |file| file.write(JSON.pretty_generate(todo_editions)) }
--- scripts/data_model.rb
+++ scripts/data_model.rb
@@ -1,12 +1,136 @@
 class DataModel
-  def work
+  def work_types
     {
       "Q47461344" => {
         "Mandatory" => {
           "P31" => "instance of",
           "P1476" => "title",
-          "P571" => "inception",
+          "P577" => "publication date",
           "P50" => "author",
+          "P407" => "language of work or name",
+          "P747" => "has edition or translation",
+        },
+        "Mandatory if applicable" => {
+          "P1680" => {
+            "label" => "subtitle",
+            "condition" => "if present"
+          },
+          "P767" => {
+            "label" => "contributor to the creative work or subject",
+            "condition" => "if present"
+          },
+          "P155" => {
+            "label" => "follows",
+            "condition" => "if present"
+          },
+          "P156" => {
+            "label" => "followed by",
+            "condition" => "if present"
+          },
+          "P571" => {
+            "label" => "inception",
+            "condition" => "if known"
+          }
+        },
+        "Optional" => {
+          "P136" => "genre",
+          "P135" => "movement",
+          "P921" => "main subject",
+          "P674" => "characters",
+          "P840" => "narrative location",
+          "P144" => "based on",
+          "P941" => "inspired by"
+        }
+      },
+      "Q7725634" => {
+        "Mandatory" => {
+          "P31" => "instance of",
+          "P1476" => "title",
+          "P50" => "author",
+          "P407" => "language of work or name",
+          "P577" => "publication date",
+          "P747" => "has edition or translation"
+        },
+        "Mandatory if applicable" => {
+          "P1680" => {
+            "label" => "subtitle",
+            "condition" => "if present"
+          },
+          "P767" => {
+            "label" => "contributor to the creative work or subject",
+            "condition" => "if present"
+          },
+          "P155" => {
+            "label" => "follows",
+            "condition" => "if present"
+          },
+          "P156" => {
+            "label" => "followed by",
+            "condition" => "if present"
+          },
+          "P571" => {
+            "label" => "inception",
+            "condition" => "if known"
+          }
+        },
+        "Optional" => {
+          "P136" => "genre",
+          "P135" => "movement",
+          "P921" => "main subject",
+          "P674" => "characters",
+          "P840" => "narrative location",
+          "P144" => "based on",
+          "P941" => "inspired by"
+        }
+      },
+      "Q35760" => {
+        "Mandatory" => {
+          "P31" => "instance of",
+          "P1476" => "title",
+          "P577" => "publication date",
+          "P50" => "author",
+          "P407" => "language of work or name",
+          "P747" => "has edition or translation"
+        },
+        "Mandatory if applicable" => {
+          "P1680" => {
+            "label" => "subtitle",
+            "condition" => "if present"
+          },
+          "P767" => {
+            "label" => "contributor to the creative work or subject",
+            "condition" => "if present"
+          },
+          "P155" => {
+            "label" => "follows",
+            "condition" => "if present"
+          },
+          "P156" => {
+            "label" => "followed by",
+            "condition" => "if present"
+          },
+          "P571" => {
+            "label" => "inception",
+            "condition" => "if known"
+          }
+        },
+        "Optional" => {
+          "P136" => "genre",
+          "P135" => "movement",
+          "P921" => "main subject",
+          "P674" => "characters",
+          "P840" => "narrative location",
+          "P144" => "based on",
+          "P941" => "inspired by"
+        }
+      },
+      "Q725377" => {
+        "Mandatory" => {
+          "P31" => "instance of",
+          "P1476" => "title",
+          "P577" => "publication date",
+          "P50" => "author",
+          "P747" => "has edition or translation",
           "P407" => "language of work or name"
         },
         "Mandatory if applicable" => {
@@ -25,6 +149,10 @@ class DataModel
           "P156" => {
             "label" => "followed by",
             "condition" => "if present"
+          },
+          "P571" => {
+            "label" => "inception",
+            "condition" => "if known"
           }
         },
         "Optional" => {
@@ -36,11 +164,155 @@ class DataModel
           "P144" => "based on",
           "P941" => "inspired by"
         }
+      },
+      "Q8261" => {
+        "Mandatory" => {
+          "P31" => "instance of",
+          "P1476" => "title",
+          "P577" => "publication date",
+          "P50" => "author",
+          "P407" => "language of work or name",
+          "P747" => "has edition or translation",
+        },
+        "Mandatory if applicable" => {
+          "P1680" => {
+            "label" => "subtitle",
+            "condition" => "if present"
+          },
+          "P767" => {
+            "label" => "contributor to the creative work or subject",
+            "condition" => "if present"
+          },
+          "P155" => {
+            "label" => "follows",
+            "condition" => "if present"
+          },
+          "P156" => {
+            "label" => "followed by",
+            "condition" => "if present"
+          },
+          "P571" => {
+            "label" => "inception",
+            "condition" => "if known"
+          }
+        },
+        "Optional" => {
+          "P136" => "genre",
+          "P135" => "movement",
+          "P921" => "main subject",
+          "P674" => "characters",
+          "P840" => "narrative location",
+          "P144" => "based on",
+          "P941" => "inspired by"
+        }
+      },
+      "Q122731938" => {
+        "Mandatory" => {
+          "P31" => "instance of",
+          "P629" => "edition or translation of",
+          "P577" => "publication date",
+          "P123" => "publisher",
+          "P407" => "language of work or name",
+          "P2438" => "narrator",
+          "P2047" => "duration"
+        },
+        "Mandatory if applicable" => {
+          "P2679" => {
+            "label" => "author of foreword",
+            "condition" => "if present"
+          },
+          "P2680" => {
+            "label" => "author of afterword",
+            "condition" => "if present"
+          },
+          "P98" => {
+            "label" => "editor",
+            "condition" => "if present"
+          },
+        },
+        "Mandatory if different from work" => {
+          "P50" => "author",
+          "P1476" => "title",
+          "P1680" => "subtitle",
+        },
+        "Optional" => {
+          "P655" => "translator"
+        }
+      },
+      "Q3331189" => {
+        "Mandatory" => {
+          "P31" => "instance of",
+          "P629" => "edition or translation of",
+          "P1476" => "title",
+          "P291" => "place of publication",
+          "P577" => "publication date",
+          "P123" => "publisher",
+          "P407" => "language of work or name"
+        },
+        "Mandatory if applicable" => {
+          "P2679" => {
+            "label" => "author of foreword",
+            "condition" => "if present"
+          },
+          "P2680" => {
+            "label" => "author of afterword",
+            "condition" => "if present"
+          },
+          "P98" => {
+            "label" => "editor",
+            "condition" => "if present"
+          },
+        },
+        "Mandatory if different from work" => {
+          "P50" => "author",
+          "P1476" => "title",
+          "P1680" => "subtitle",
+        },
+        "Optional" => {
+          "P655" => "translator",
+          "P110" => "illustrator",
+          "P872" => "printed by",
+        }
+      },
+      "Q1224889" => {
+        "Mandatory" => {
+          "P31" => "instance of",
+          "P629" => "edition or translation of",
+          "P1476" => "title",
+          "P291" => "place of publication",
+          "P577" => "publication date",
+          "P123" => "publisher",
+          "P407" => "language of work or name"
+        },
+        "Mandatory if applicable" => {
+          "P2679" => {
+            "label" => "author of foreword",
+            "condition" => "if present"
+          },
+          "P2680" => {
+            "label" => "author of afterword",
+            "condition" => "if present"
+          },
+          "P98" => {
+            "label" => "editor",
+            "condition" => "if present"
+          },
+        },
+        "Mandatory if different from work" => {
+          "P50" => "author",
+          "P1476" => "title",
+          "P1680" => "subtitle",
+        },
+        "Optional" => {
+          "P655" => "translator",
+          "P110" => "illustrator",
+          "P872" => "printed by",
+        }
       }
     }
   end
 
-  def edition
+  def edition_types
     {
       "Q3331189" => {
         "Mandatory" => {
@@ -74,7 +346,6 @@ class DataModel
           "P655" => "translator",
           "P110" => "illustrator",
           "P872" => "printed by",
-          "P655" => "translator"
         }
       }
     }
