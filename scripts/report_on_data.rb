8e9ced8860d5b0712e3e2e8263d098133debe1ee
Huw Diprose <mail@huwdiprose.co.uk>
2025-11-11 17:38:13 +0000

    Fix: DataReport
    Woops forgot to modify this after I moved  from an Array to a Hash
    
    Serves me right for YOLO'ing this without tests

--- scripts/report_on_data.rb
+++ scripts/report_on_data.rb
@@ -9,21 +9,21 @@ class DataReport
   end
 
   def books_missing_works
-    @books_missing_works ||= @scraped_books.reject do |book|
-      complete_wikidata_iri?(book["work_iri"])
+    @books_missing_works ||= @scraped_books.reject do |book_key|
+      complete_wikidata_iri?(@scraped_books[book_key]["work_iri"])
     end
   end
 
   def books_edition_works
-    @books_missing_works ||= @scraped_books.reject do |book|
-      complete_wikidata_iri?(book["edition_iri"])
+    @books_missing_works ||= @scraped_books.reject do |book_key|
+      complete_wikidata_iri?(@scraped_books[book_key]["edition_iri"])
     end
   end
 
   def data_assembler
     @scraped_book_count = @scraped_books.count
     @missing_work_count = books_missing_works.count
-    @potential_work_match_count = books_missing_works.count { |book| book["wikidata_potential_work_iris"]&.any? }
+    @potential_work_match_count = books_missing_works.keys.count { |book_key| books_missing_works[book_key]["wikidata_potential_work_iris"]&.any? }
     @missing_work_percentage = ((@missing_work_count.to_f / @scraped_book_count) * 100).round(2)
     @missing_edition_count = books_edition_works.count
     @missing_edition_percentage = ((@missing_edition_count.to_f / @scraped_book_count) * 100).round(2)
