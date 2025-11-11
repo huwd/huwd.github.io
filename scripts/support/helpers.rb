6512a05cbaa132835fecc33e8a163f0c96a0e3c0
Huw Diprose <mail@huwdiprose.co.uk>
2025-11-11 17:03:08 +0000

    Add a Helper that we can include across all wikidata scripts
    Currently I'm going to import my wikidata adaptor lib locally.
    That'll need to be changed to a RubyGems option once it's released

---
+++ scripts/support/helpers.rb
@@ -0,0 +1,26 @@
+require 'yaml'
+require_relative '../../../wikidata_adaptor/lib/wikidata_adaptor'
+
+module Helpers
+  class UnknownInstanceType < StandardError; end
+
+  def load_scraped_books
+    JSON.parse(File.read('_data/scraped_books.json'))
+  end
+
+  def load_stored_books
+    Dir.glob("_books/*.md").each_with_object({}) do |file, acc|
+      frontmatter = File.read(file)[/\A---\s*\n(.*?)\n---/m, 1]
+      acc[file] = YAML.safe_load(frontmatter || "", permitted_classes: [Time], aliases: true) || {}
+    end
+  end
+
+  # Treat only Q followed by digits as complete, e.g. https://www.wikidata.org/wiki/Q55360383
+  def complete_wikidata_iri?(iri)
+    iri.is_a?(String) && iri.strip.match?(%r{\Ahttps?://(?:www\.)?wikidata\.org/(?:wiki|entity)/Q\d+\z}i)
+  end
+
+  def wikidata_rest_client
+    WikidataAdaptor.rest_api
+  end
+end
